1c6f5fdde7efa48ec15a718661782fbccb41d65e
T Lucas <107696223+TamLucas@users.noreply.github.com>
2023-03-16 10:47:10 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Tester_cs
+{
+
+    class Circle
+    {
+
+        //Properties
+        private double Radius;
+        private double Area;
+        private double Circumference;
+        private const double PI = Math.PI;
+
+        //Constructor
+        public Circle(double Radius)
+        {
+            this.Radius = Radius;
+            this.Area = 0.0;
+            this.Circumference = 0.0;
+        }
+        //Methods
+        public void GetArea()
+        {
+                  
+            this.Area = PI * Math.Pow(Area, 2);
+        }
+
+        public void GetCircumference()
+        {
+            this.Circumference = 2 * PI * Radius;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Circle Information:");
+            Console.WriteLine($"Radius: {Radius}");
+            Console.WriteLine($"Area: {Area}");
+            Console.WriteLine($"Circumference: {Circumference}");
+           
+        }
+        //..................
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Circle C1 = new Circle(5.0);
+                C1.GetArea();
+                C1.GetCircumference();
+                C1.PrintInfo();
+            }
+        }
+    }
+
+}
